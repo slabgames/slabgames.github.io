2cdfa4c2fe1c982129e7e2d0759e09675a3a318f
Hermanto <mamankuat@gmail.com>
2020-08-15 15:53:52 +0800

    upate 1.3.9g

--- code2.js
+++ code2.js
@@ -225,7 +225,7 @@ gdjs.QuizCode.GDTimeElapsedObjects1.createFrom(runtimeScene.getObjects("TimeElap
 }
 
 
-};gdjs.QuizCode.userFunc0x72ff88 = function(runtimeScene, objects) {
+};gdjs.QuizCode.userFunc0x71c778 = function(runtimeScene, objects) {
 "use strict";
 
 const QUESTION = [
@@ -371,7 +371,7 @@ gdjs.QuizCode.GDQuestionObjects1.createFrom(runtimeScene.getObjects("Question"))
 
 var objects = [];
 objects.push.apply(objects,gdjs.QuizCode.GDQuestionObjects1);
-gdjs.QuizCode.userFunc0x72ff88(runtimeScene, objects);
+gdjs.QuizCode.userFunc0x71c778(runtimeScene, objects);
 
 }
 
